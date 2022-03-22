3768322a7d77f42741a92bec9558d63c7a98e0dc
mikeyuku <mikeyuko@gmail.com>
2022-03-22 11:25:24 +0800

    v01

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -3,4 +3,12 @@ import 'package:dart_application_1/dart_application_1.dart'
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_application_1.calculate()}!');
+
+  List xxx = ['123', '234', '345'];
+  List yyy = ['aaaa', 'bbbb', 'cccc'];
+  Map mmap = {};
+  for (var ii = 0; ii < xxx.length; ii++) {
+    mmap.addAll({xxx[ii]: yyy[ii]});
+  }
+  print(mmap);
 }
